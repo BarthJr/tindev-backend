27300739528bf0f064c345590515d227bc5baf01
Barth <juniior.barth@gmail.com>
2019-08-09 12:58:03 -0300

    Implement socket to match devs in realtime

--- src/server.js
+++ src/server.js
@@ -3,18 +3,33 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 
-
 const routes = require('./routes')
 
-const server = express()
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+
+const connectedUsers = {}
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query
+  connectedUsers[user] = socket.id
+})
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0-qehev.mongodb.net/test?retryWrites=true&w=majority`, {
   useNewUrlParser: true
 })
 
-server.use(cors())
-server.use(express.json())
-server.use(routes)
+
+app.use((req, res, next) => {
+  req.io = io
+  req.connectedUsers = connectedUsers
+
+  return next()
+})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 server.listen(3333)
 
